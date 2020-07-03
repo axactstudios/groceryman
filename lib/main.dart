a120cb768c7d199fad94fd72f3d573b7de9832c5
Chitransh Jain <cjsmcs2810@gmail.com>
2020-07-03 18:22:47 +0530

    set home page to welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:groceryman/Drawer/MainHome.dart';
+import 'package:groceryman/LoginPages/WelcomeScreen.dart';
 
 void main() async {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: MainHome(),
+      home: WelcomeScreen(),
     );
   }
 }
